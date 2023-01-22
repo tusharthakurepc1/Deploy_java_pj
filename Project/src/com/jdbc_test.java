a366885e643b5255d6e474c1ae62aced863a3c28
Tushar <tusharthakurepc205@gmail.com>
2023-01-22 15:35:26 +0530

    v1.0.3

---
+++ Project/src/com/jdbc_test.java
@@ -0,0 +1,17 @@
+package com;
+
+import java.sql.*;
+
+public class jdbc_test {
+    public static void main(String args[]){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/java_projects","root","epc205");
+            System.out.println("Connection Open");
+            con.close();
+        }catch(Exception e){
+            System.out.println("Exception.");
+            System.out.println(e);
+        }
+    }
+}
